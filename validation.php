e7a1125034c6c71840ec9bf70599deaae3dfe013
Emeric Levasseur <levasseur.emeric@outlook.fr>
2016-09-14 16:41:14 +0200

    register.php

--- validation.php
+++ validation.php
@@ -3,12 +3,40 @@ require('config/config.php');
 require('model/functions.fn.php');
 session_start();
 
-if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
+if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) &&
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
 
-}else{ 
-	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
-	header('Location: register.php');
-}
+
+
+		if(isset($_POST) && !empty($_POST)){
+				$available = isUsernameAvailable($db, $_POST['username']);
+				if($available == false)
+				{
+					$_SESSION['message'] = 'Erreur : Username invalide';
+
+					header('Location: register.php');
+				}
+			}
+
+			if(isset($_POST) && !empty($_POST && $available)){
+					$availables = isEmailAvailable($db, $_POST['email']);
+					if($availables == false)
+					{
+						$_SESSION['message'] = 'Erreur : Email invalide';
+
+						header('Location: register.php');
+					}
+				}
+
+			}else{
+				$_SESSION['message'] = 'Erreur : Formulaire incomplet';
+				header('Location: register.php');
+			}
+
+				if(isset($_POST) && !empty($_POST)){
+					if ($available && $availables){
+						$inscrit = userRegistration($db, $_POST['username'],  $_POST['email'],  $_POST['password']);
+							header('Location: dashboard.php');
+					}
+				}
